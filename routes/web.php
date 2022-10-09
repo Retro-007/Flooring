555c8d15c448fb4a7e6e88fcd9a15dd8e0f8af2c
roufan7 <muhammedroufan@gmail.com>
2022-10-09 22:43:05 +0530

    assets anf migration

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.layout.master');
 });
 
 Route::get('/dashboard', function () {
